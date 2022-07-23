7c51ecd217cfd32d61f1a35f1e7b3f524b31ba1a
Vitor Rocha <vitorgrocha@gmail.com>
2022-07-23 17:31:13 -0300

    Cors

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,13 @@ var authenticate = require('../authenticate');
 
 
 const Users = require('../models/user');
+const cors = require('./cors');
 
 var router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get("/", authenticate.verifyAdmin, function (req, res, next) {
+router.get("/", cors.corsWithOptions, authenticate.verifyAdmin, function (req, res, next) {
   User.find({})
     .then(
       (users) => {
@@ -24,7 +25,7 @@ router.get("/", authenticate.verifyAdmin, function (req, res, next) {
     .catch((err) => next(err));
 });
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup',cors.corsWithOptions, (req, res, next) => {
   User.register(new User({ username: req.body.username }),
     req.body.password, (err, user) => {
       if (err) {
@@ -56,7 +57,7 @@ router.post('/signup', (req, res, next) => {
     });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login',cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
 
   var token = authenticate.getToken({ _id: req.user._id });
   res.statusCode = 200;
